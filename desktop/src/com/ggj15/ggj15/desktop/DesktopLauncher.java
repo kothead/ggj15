5d2cda03680ef4b1baf49d0b2e6427b0b07479c6
U-jhaampi\kettricken <kettricken@jhaampi.(none)>
2015-01-23 23:58:47 +0600

    first commit

---
+++ desktop/src/com/ggj15/ggj15/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ggj15.ggj15.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ggj15.ggj15.GGJGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GGJGame(), config);
+	}
+}
