93c5f57a6277f2e5b477fe28470534b86f1134fa
NtsDK <ghoul71@yandex.ru>
2015-01-31 04:34:06 +0500

    Sync rocket to planet.

--- core/src/com/ggj15/model/Rocket.java
+++ core/src/com/ggj15/model/Rocket.java
@@ -3,6 +3,7 @@ package com.ggj15.model;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.ggj15.data.Configuration;
 import com.ggj15.data.ImageCache;
@@ -14,18 +15,26 @@ public class Rocket extends Sprite {
 
     private RocketActor actor;
 
-    public Rocket() {
+    private Planet planet;
+
+    public Rocket(Planet planet) {
         setRegion(ImageCache.getTexture("rocket"));
         setSize(getRegionWidth(), getRegionHeight());
         setOrigin(getRegionWidth() / 2f, getRegionHeight() / 2f);
 
         actor = new RocketActor();
+        this.planet = planet;
     }
 
     @Override
     public void draw(Batch batch) {
         super.draw(batch);
         batch.draw(Rocket.this, getX(), getY());
+    }
+
+    public void process(float delta){
+        this.setX(planet.getCleanX() + planet.getCleanWidth()/2);
+        this.setY(planet.getCleanY() + planet.getCleanHeight());
     }
 
     public RocketActor getActor() {
